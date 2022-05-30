0db855bfd18655c6585d3589f00e29f306cc567c
dabirax <92556526+dabirax@users.noreply.github.com>
2022-05-30 02:04:21 +0100

    Add files via upload

---
+++ coolpines/js/script.js
@@ -0,0 +1,89 @@
+// JavaScript source
+function desc() {
+     x = document.getElementById('desc');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function desc2() {
+     x = document.getElementById('desc2');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function desc3() {
+     x = document.getElementById('desc3');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function desc4() {
+     x = document.getElementById('desc4');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function desc5() {
+     x = document.getElementById('desc5');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function desc6() {
+     x = document.getElementById('desc6');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function desc7() {
+     x = document.getElementById('desc7');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function desc8() {
+     x = document.getElementById('desc8');
+    x.style.opacity = '0.8';
+    x.style.animation = 'Tranimation, Opanimation2';
+    x.style.animationIterationCount = '1';
+    x.style.animationDuration = '1s';
+}
+function undesc() {
+   x = document.getElementById("desc");
+    x.style.opacity = "0";
+}
+function undesc2() {
+   x = document.getElementById("desc2");
+    x.style.opacity = "0";
+}
+function undesc3() {
+   x = document.getElementById("desc3");
+    x.style.opacity = "0";
+}
+function undesc4() {
+   x = document.getElementById("desc4");
+    x.style.opacity = "0";
+}
+function undesc5() {
+   x = document.getElementById("desc5");
+    x.style.opacity = "0";
+}
+function undesc6() {
+   x = document.getElementById("desc6");
+    x.style.opacity = "0";
+}
+function undesc7() {
+   x = document.getElementById("desc7");
+    x.style.opacity = "0";
+}
+function undesc8() {
+   x = document.getElementById("desc8");
+    x.style.opacity = "0";
+}
